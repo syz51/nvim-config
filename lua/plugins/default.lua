408072b88bae3fbd3091bd241d44c34711dabef8
ysun11 <Roy.Sun@aexp.com>
2024-02-01 23:46:20 +0000

    add dap for debugging

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -3,6 +3,23 @@ return {
     "vuki656/package-info.nvim",
     ft = "json",
     dependencies = "MunifTanjim/nui.nvim",
-    opts = {},
+    opts = {
+      colors = {
+        up_to_date = "#a3be8c",
+        outdated = "#bf616a",
+      },
+    },
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    keys = {
+      {
+        "<leader>sf",
+        function()
+          require("spectre").open_file_search()
+        end,
+        desc = "Find In Current File",
+      },
+    },
   },
 }
