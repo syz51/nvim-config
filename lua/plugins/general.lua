9b27fcfab203e6ce7d0ba2b028ac30a95630f2e9
syz51 <syzroy@gmail.com>
2024-08-07 01:29:01 +0100

    fix package-info color problem

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -3,7 +3,28 @@ return {
     "vuki656/package-info.nvim",
     ft = "json",
     dependencies = "MunifTanjim/nui.nvim",
-    opts = {},
+    keys = {
+      {
+        "<leader>ns",
+        function()
+          require("package-info").show({ force = true })
+        end,
+        desc = "Force Refresh Package Info",
+      },
+    },
+    opts = {
+      colors = {
+        up_to_date = "#98c379", -- Text color for up to date dependency virtual text
+        outdated = "#d19a66", -- Text color for outdated dependency virtual text
+      },
+    },
+    config = function(_, opts)
+      require("package-info").setup(opts)
+
+      -- manually register them
+      vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
+      vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
+    end,
   },
 
   {
